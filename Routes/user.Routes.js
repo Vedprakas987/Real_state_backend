eef53820f53be5c3c40dcaaeea7a9f9880767b46
Vedprakash Sinha <vedprakash4gs@gmail.com>
2023-05-15 03:13:15 +0530

    added new file

--- Routes/user.Routes.js
+++ Routes/user.Routes.js
@@ -1,34 +1,48 @@
-const express = require("express")
-const userRouter=express.Router()
-userRouter.use(express.json())
-const bcrypt = require("bcrypt")
-const jsw = require("jsonwebtoken")
-const { userModel } = require("../Model/user.model")
-userRouter.get("/",(req,res)=>{
-    res.send("welcome to users")
-})
-userRouter.post("/register",(req,res)=>{
-    const {name,email,password,isAdmin} = req.body
-    bcrypt.hash(password,3,function(err,hash){
-        const user_new =new userModel({name:name,email:email,password:hash,isAdmin:isAdmin})
-        user_new.save()
-        res.send({"msg":"user has been registered"})
-    })
-    console.log(req.body)
-})
-userRouter.post("/login",async(req,res)=>{
-    const {email,password} = req.body
-    const user = await userModel.find({email})
-    if(user){
-        bcrypt.compare(password,user[0].password,function(err,result){
-            if(result){
-                res.send({"msg":"Your are Loged in",token:jsw.sign({"userid":user[0]._id},"ved")})
-            }else{
-                res.send({"msg":"please fill the correct credentials"})
-            }
-        })
+const express = require("express");
+const userRouter = express.Router();
+const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
+const { userModel } = require("../Model/user.model");
+
+userRouter.get("/", (req, res) => {
+  res.send("Welcome to users");
+});
+
+userRouter.post("/register", async (req, res) => {
+  try {
+    const { name, email, password, isAdmin } = req.body;
+    const hashedPassword = await bcrypt.hash(password, 10);
+    const user = new userModel({
+      name: name,
+      email: email,
+      password: hashedPassword,
+      isAdmin: isAdmin,
+    });
+    await user.save();
+    res.send({ message: "User has been registered" });
+  } catch (error) {
+    res.status(500).send({ message: "An error occurred while registering the user" });
+  }
+});
+
+userRouter.post("/login", async (req, res) => {
+  try {
+    const { email, password } = req.body;
+    const user = await userModel.findOne({ email });
+    if (!user) {
+      return res.status(401).send({ message: "Invalid credentials" });
     }
-})
-module.exports={
-    userRouter
-}
+    const passwordMatch = await bcrypt.compare(password, user.password);
+    if (!passwordMatch) {
+      return res.status(401).send({ message: "Invalid credentials" });
+    }
+    const token = jwt.sign({ userId: user._id }, "secretKey");
+    res.send({ message: "You are logged in", token });
+  } catch (error) {
+    res.status(500).send({ message: "An error occurred while logging in" });
+  }
+});
+
+module.exports = {
+  userRouter,
+};
